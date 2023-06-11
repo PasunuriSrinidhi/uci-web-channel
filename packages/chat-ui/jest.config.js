521a9fe87296f6b07c09091df53bbcf14a483052
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-06-11 15:11:11 +0530

    Update jest.config.js
    Enabling code coverage collection, specifying the coverage directory, customizing module resolution directories, and adding a transformation for JavaScript and TypeScript files using Babel.

--- packages/chat-ui/jest.config.js
+++ packages/chat-ui/jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/lib/', '/es/', '/dist/', 'examples'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  collectCoverage: true, // Enable code coverage collection
+  coverageDirectory: 'coverage', // Specify the directory for coverage reports
+  moduleDirectories: ['node_modules'], // Customize module resolution directories if needed
+  transform: {
+    // Add additional transformations if required
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
 };
